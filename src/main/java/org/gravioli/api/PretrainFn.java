c85e0c4eebba63b74628c872b71ea16ca940354d
Tony Zhang <45370652+tnyz@users.noreply.github.com>
2020-09-27 22:15:13 -0700

    cloud function

--- src/main/java/org/gravioli/api/PretrainFn.java
+++ src/main/java/org/gravioli/api/PretrainFn.java
@@ -3,16 +3,49 @@ package org.gravioli.api;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
 import com.google.cloud.functions.HttpResponse;
+import com.google.gson.*;
 import org.gravioli.Runner;
-import org.gravioli.io.Geo;
+import org.gravioli.core.Geo;
+import scala.jdk.javaapi.CollectionConverters;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PretrainFn implements HttpFunction {
-    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
-        Runner<Geo> runner = new Runner<Geo>()
 
+    private static final Gson gson = new Gson();
+
+    public void service(HttpRequest httpRequest, HttpResponse httpResponse) throws Exception {
+        Map<String, List<String>> qp = httpRequest.getQueryParameters();
+        System.out.println("query parameters: " + qp);
+
+        ArrayList<Geo> points = validateInputAndExtract(httpRequest);
+        System.out.println(points.size());
+        String response = Runner.runLocalZone("", CollectionConverters.asScala(points));
         BufferedWriter writer = httpResponse.getWriter();
-        writer.write("Instance execution count: ");
+        writer.write(response);
+    }
+
+    private ArrayList<Geo> validateInputAndExtract(HttpRequest httpRequest) {
+        ArrayList<Geo> geoData = new ArrayList<>();
+        try {
+            JsonElement requestParsed = gson.fromJson(httpRequest.getReader(), JsonElement.class);
+            JsonObject requestJson = requestParsed.getAsJsonObject();
+
+            JsonArray locations = requestJson.getAsJsonArray("locations");
+            for (JsonElement point : locations) {
+                JsonObject pointObject = point.getAsJsonObject();
+                Double lat = pointObject.get("latitude").getAsDouble();
+                Double lon = pointObject.get("longitude").getAsDouble();
+                Long ts = pointObject.get("timestamp").getAsLong();
+                geoData.add(new Geo(lat, lon, ts));
+            }
+        } catch (JsonParseException | IOException e) {
+            System.out.println("Error parsing JSON: " + e.getMessage());
+        }
+        return geoData;
     }
 }
